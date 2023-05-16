85ed85653dbb5ecacf02c1ee320ee5df5b79f1ba
JoseMSan <jsantp01@estudiantes.unileon.es>
2023-05-16 11:54:55 +0200

    oasuldhfldsij

--- backend/index.js
+++ backend/index.js
@@ -9,9 +9,16 @@ var app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use("/login", require("./routes/login"));
-app.use("/", require("./routes/cards"));
+app.use("/api/login", require("./routes/login"));
+app.use("/api", require("./routes/cards"));
 
 app.listen(5000, function () {
   console.log("Servidor arrancado en el puerto 5000!");
 });
+
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static("../frontend/build"));
+  app.get("*", (req,res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"));
+  });
+}
